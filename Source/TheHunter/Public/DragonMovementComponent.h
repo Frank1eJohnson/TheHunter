78dd1e207a33660ab3a357abdc45ec56ae5c91bf
Zhongqi Shan <60977360+Sabrave@users.noreply.github.com>
2018-01-02 13:25:56 +0800

    121212

---
+++ Source/TheHunter/Public/DragonMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "DragonMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEHUNTER_API UDragonMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
